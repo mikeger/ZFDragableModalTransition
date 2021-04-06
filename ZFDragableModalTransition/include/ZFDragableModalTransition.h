1ca59cd6c97f3c7bd0fe94d2dfd893833ddc56b0
Peter <peterli@163.com>
2021-04-06 14:49:41 +0800

    move header to include/

--- ZFDragableModalTransition/include/ZFDragableModalTransition.h
+++ ZFDragableModalTransition/include/ZFDragableModalTransition.h
@@ -14,10 +14,6 @@ FOUNDATION_EXPORT double ZFDragableModalTransitionVersionNumber;
 //! Project version string for ZFDragableModalTransition.
 FOUNDATION_EXPORT const unsigned char ZFDragableModalTransitionVersionString[];
 
-#ifdef __OBJC__
-  #import "ZFModalTransitionAnimator.h"
-#endif
-
 // In this header, you should import all the public headers of your framework using statements like #import <ZFDragableModalTransition/PublicHeader.h>
 
-#import <ZFDragableModalTransition/ZFModalTransitionAnimator.h>
+#import <ZFModalTransitionAnimator.h>
