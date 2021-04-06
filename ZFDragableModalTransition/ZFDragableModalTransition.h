765e161d553230529663df3232562bb220955729
Peter <peterli@163.com>
2021-04-06 14:46:19 +0800

    tweak header

--- ZFDragableModalTransition/ZFDragableModalTransition.h
+++ ZFDragableModalTransition/ZFDragableModalTransition.h
@@ -20,4 +20,4 @@ FOUNDATION_EXPORT const unsigned char ZFDragableModalTransitionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZFDragableModalTransition/PublicHeader.h>
 
-
+#import <ZFDragableModalTransition/ZFModalTransitionAnimator.h>
